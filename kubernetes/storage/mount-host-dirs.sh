ef602a6236171682440c73ae3213ec9653203504
Thompson, Darren (IM&T, Clayton) <Darren.Thompson@csiro.au>
2021-10-06 13:11:22 +1100

    Added kubernetes deployment

---
+++ kubernetes/storage/mount-host-dirs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# minikube mounts
+minikube mount /home/tho78s/development/ANSTO/areaDetectorZMQ-docker/test:/test &
+minikube mount /home/tho78s/development/ANSTO/areaDetectorZMQ-docker/notebooks:/jupyter-notebooks &
